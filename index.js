4a18c15a805527bb658028a74feedde623c50cae
Alex Ferro <alex.ferreira.ferro@gmail.com>
2024-04-29 13:50:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 document.getElementById("rsvp-form").addEventListener("submit", function(event) {
   event.preventDefault();
-  
+
   // Obter os valores dos campos
   var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
   var attendance = document.getElementById("attendance").value;
-  
-  // Aqui você pode enviar os dados para o servidor, armazenar em um banco de dados, ou enviar por e-mail
-  console.log("Nome: " + name);
-  console.log("E-mail: " + email);
-  console.log("Confirmação de Presença: " + attendance);
-  
-  // Você também pode exibir uma mensagem de confirmação para o usuário
-  alert("Obrigado por confirmar sua presença, " + name + "!");
+
+  // Criar um objeto com os dados do formulário
+  var formData = new FormData();
+  formData.append("Nome", name); 
+  formData.append("Confirmação de Presença", attendance);
+
+  // Enviar os dados para o servidor
+  fetch('https://forms.gle/pJ5btJLUFXpcEUzd6', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao enviar os dados');
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Exibir uma mensagem de confirmação para o usuário
+    alert("Obrigado por confirmar sua presença, " + name + "!");
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
 });
