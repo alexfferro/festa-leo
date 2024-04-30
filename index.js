5e81573c16e8f35d01eaf3f1cff8532148c396d3
Alex Ferro <alex.ferreira.ferro@gmail.com>
2024-04-30 11:48:50 -0300

    atualizacao do codigo fonte

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 document.getElementById("rsvp-form").addEventListener("submit", function(event) {
   event.preventDefault();
 
-  // Obter o ID do formulário do Google Forms
-  var formId = document.getElementById("form-id").value;
-
-  // Obter os valores dos campos
   var name = document.getElementById("name").value;
   var attendance = document.getElementById("attendance").value;
 
-  // Enviar os dados para o Google Forms
-  fetch(`https://docs.google.com/forms/d/e/${formId}/formResponse`, {
-    method: 'POST',
-    mode: 'no-cors',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `T2Ybvb2=${encodeURIComponent(name)}&T2Ybvb4=${attendance}`
+  var data = {
+    name: name,
+    attendance: attendance
+  };
+
+  fetch('.github/rsvp.json', {
+    method: 'GET'
   })
-  .then(() => {
-    // Exibir uma mensagem de confirmação para o usuário
-    alert("Obrigado por confirmar sua presença, " + name + "!");
-    // Limpar os campos do formulário
-    document.getElementById("name").value = "";
-    document.getElementById("attendance").value = "Sim"; // Defina o valor padrão conforme necessário
+  .then(response => response.json())
+  .then(json => {
+    json.push(data);
+
+    fetch('.github/rsvp.json', {
+      method: 'PUT',
+      body: JSON.stringify(json)
+    })
+    .then(() => {
+      alert("Obrigado por confirmar sua presença, " + name + "!");
+    })
+    .catch(error => {
+      console.error('Erro ao salvar os dados:', error);
+    });
   })
   .catch(error => {
-    console.error('Erro:', error);
+    console.error('Erro ao recuperar os dados:', error);
   });
 });
